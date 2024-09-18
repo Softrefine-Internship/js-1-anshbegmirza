e77d9741ebfb6955fde0c93ca2143472b7f177a4
anshbegmirza <anshbegmirza@gmail.com>
2024-09-18 17:41:34 +0530

    Changes Completed

--- program-7.js
+++ program-7.js
@@ -22,14 +22,44 @@ const array = [
   { title: "Java", author: "James" },
 ];
 
+const ip2 = [
+  { title: "JS", author: "Bjarne" },
+  { title: "C#", author: "James" },
+  { title: "C++", author: "James" },
+  { title: "C++", author: "James" },
+];
 
+const removeDuplicates = function (array) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    let isUnique = false;
+
+    for (let j = 0; j < result.length; j++) {
+      if (array[i].title === result[j].title) {
+        isUnique = true;
+        break;
+      }
+    }
+    if (!isUnique) {
+      result.push(array[i]);
+    }
+  }
+  return result;
+}
+
+console.log(removeDuplicates(array));
+console.log(`Second Input`);
+console.log(removeDuplicates(ip2));
+
+
+
+// Method 2
+/*
 //using filter method
-
 const uniqueElements = array.filter(
   (obj, index) => {
     return index === array.findIndex(o => obj.title === o.title);
   }
 
 );
-
-console.log(uniqueElements);
+*/
